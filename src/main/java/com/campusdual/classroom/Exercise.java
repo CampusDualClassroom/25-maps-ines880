50d579eba683cff1dd132ce8cdd3c4eb465716c9
Inés <ines.ramos@campusdual.com>
2025-11-03 17:13:23 +0100

    solved exercise 25

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -1,18 +1,31 @@
 package com.campusdual.classroom;
 
+import java.security.Key;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Exercise {
 
     public static Map<String, Person> createHashMap() {
+        Map<String, Person> map = new HashMap<>();
 
-    }
+        map.put("person", new Person("John", "Smith"));
+        map.put("teacher", new Teacher("María", "Montessori", "Educación"));
+        map.put("police", new PoliceOfficer("Jake", "Peralta", "B-99"));
+        map.put("doctor", new Doctor("Gregory", "House", "Nefrología e infectología"));
+        return map;
+            }
 
     public static Person addMapValue(Map<String, Person> map, String key, Person value) {
+        map.put(key, value);
+        return value;
 
     }
 
     public static void printMapValues(Map<String, Person> map) {
+        for (Map.Entry<String, Person> entry : map.entrySet()){
+            entry.getValue().getDetails();
+        }
 
     }
 
